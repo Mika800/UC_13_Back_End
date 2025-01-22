2a2efe3456dbce42ab24c70e1008b60feeba5ea8
EMILLY VITORIA CEZARINO DA SILVA <emilly.vcsilva1@senacsp.edu.br>
2025-01-21 22:16:55 -0300

    Aula 21-01

--- app.js
+++ app.js
@@ -1,34 +1,19 @@
-import carros2024 from './tabelacarros.js';
-import express from 'express';
+// Importa o framework Express, usado para criar o servidor e gerenciar rotas
+import express from "express";
+// Importa as rotas relacionadas a carros (carroRoutes.js)
+import carroRoutes from "./routes/carroRoutes.js";
 
-const app = express ();
+// Cria uma instância do aplicativo Express
+const app = express();
 
+// Middlewares
 app.use(express.json());
 
-app.get('/',(resisicao,resposta) => {
-    resposta.status(200).send(carros2024);
-});
+// Rotas
+app.use("/carros", carroRoutes);
 
-app.get('/:sigla', (requisicao, resposta) => {
-    const siglaInformada = requisicao.params.sigla.toUpperCase(); // Obtém a sigla do parâmetro da URL.
-    const carro = carros2024.find((infoCarro) => infoCarro.sigla === siglaInformada); // Busca o carro pela sigla.
-    if (!carro) {
-        // ! Vazio - Not
-        // Se o carro não for encontrado, retorna erro 404.
-        resposta
-        .status(404)
-        .send(
-            'Não existe um carro com a sigla informada!' // Mensagem de erro explicativa.
-        );
-     return; // Para a função
-    }
-    resposta.status(200).send(carro); // Se encontrado, retorna o carro com status 200 (OK).
+// Inicializa o servidor para executar na porta 3000
+app.listen(3000, () => {
+  // Quando o servidor for iniciado, imprime no console que ele está rodando na porta 3000
+  console.log("Servidor Rodando com Sucesso na porta 3000");
 });
-app.post('/', (req, res) => {
-    const novoCarro = req.body; // Obtém o corpo enviado para incluir um carro
-    carros2024.push(novoCarro); // Adiciona o novo carro à lista de carros.
-    res.status(200).send(novoCarro); // Retorna o carro adicionado com status 200 (Ok).
-});
-app.listen(3000,() => console.log ("Servidor Rodando com Sucesso"));
-
-//node app.js
